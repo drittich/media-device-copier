1a3e2a9088e0727a11e70acb44d31c76210532c5
D'Arcy Rittich <drittich@gmail.com>
2025-06-05 11:05:13 -0400

    Add unit test projects

---
+++ MediaDeviceCopier.Tests.Mocked/MtpDeviceMockTests.cs
@@ -0,0 +1,23 @@
+using Xunit;
+using MediaDeviceCopier;
+
+namespace MediaDeviceCopier.Tests.Mocked;
+
+public class MtpDeviceMockTests
+{
+    [Fact]
+    public void GetFiles_InvalidFolder_Throws()
+    {
+        var device = new MtpDevice(new MockMediaDevice());
+        Assert.Throws<DirectoryNotFoundException>(() => device.GetFiles("invalid"));
+    }
+
+    [Fact]
+    public void DeviceFactory_ReturnsMock()
+    {
+        MtpDevice.DeviceFactory = () => new[] { (IMediaDevice)new MockMediaDevice() };
+        var devices = MtpDevice.GetAll();
+        Assert.Single(devices);
+        Assert.Equal("MockDevice", devices[0].FriendlyName);
+    }
+}
